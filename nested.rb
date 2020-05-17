e057d72971e31058b4430b36d12345ab39f5e7a2
Jack Quinoa <jquinoa208@gmail.com>
2020-05-17 02:52:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -102,7 +102,7 @@ def changing_alan
         }
      }
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     programmer_hash[:alan_kay][:known_for]
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -122,5 +122,5 @@ def adding_to_dennis
         }
      }
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
-  programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash
 end
